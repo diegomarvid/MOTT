462d161e54c8467ff7e7e99f477c300a0c29f900
Diego Marvid <33181424+diegomarvid@users.noreply.github.com>
2021-07-19 09:52:47 -0500

    Add Simple Examples

---
+++ Examples/SimpleReciever/SimpleReciever.ino
@@ -0,0 +1,39 @@
+#include <MOTT.h>
+
+MOTT mott;
+
+const int RX_SIGNAL_PIN = 8;
+
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(RX_SIGNAL_PIN, INPUT);
+
+  mott.SetRxPin(RX_SIGNAL_PIN);
+  mott.SetBitTime(0.080L, Callback);
+
+  mott.EnableSampling();
+
+  Serial.println("-------------------------");
+  Serial.println("Ready to detect signal...");
+
+}
+
+void Callback()
+{
+  mott.HandleInterrupt();
+}
+
+char string[25];
+
+void loop() {
+  
+  if(mott.SamplingEnded())
+  {
+    mott.ObtainSample(string);
+    Serial.println(string);
+    mott.ResumeSampling();
+  } 
+
+}
---
+++ Examples/SimpleSender/SimpleSender.ino
@@ -0,0 +1,37 @@
+#include <MOTT.h>
+
+const int TX_SIGNAL_PIN = 8;
+
+MOTT mott;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(TX_SIGNAL_PIN, OUTPUT);
+
+  mott.SetTxPin(TX_SIGNAL_PIN);
+  mott.SetBitTime(0.080L, Callback);
+
+  Serial.println("Loading up...");
+
+  Serial.println("Sending signal in 3 seconds");
+  delay(3000);
+
+  const char* msg = "Hello world!";
+
+  Serial.println(msg);
+  mott.SendSignal(msg); 
+
+}
+
+void Callback()
+{
+  mott.HandleInterrupt();
+}
+
+
+void loop() {
+
+  
+ 
+}
