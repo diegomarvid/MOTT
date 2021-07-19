58bd87180ed265aad1deeea09a3eddcce57b972a
Diego Marvid <33181424+diegomarvid@users.noreply.github.com>
2021-07-19 08:58:36 -0500

    Version inicial

---
+++ MOTT.cpp
@@ -0,0 +1,291 @@
+#include "Arduino.h"
+#include "MOTT.h"
+
+
+MOTT::MOTT()
+{
+	TIMER_TIME = 1L*ms;
+	TIMER_TIME_IN_MS = TIMER_TIME / ms;
+	SIGNAL_SIZE = SIGNAL_MAX_SIZE;
+	
+	RX_SIGNAL_PIN = 7;
+	TX_SIGNAL_PIN = 8;
+	
+	i = 0;
+	sending = false;
+	
+	largo = 0;
+	reading_signal = false;
+	reading_ended = true;
+	samplingEnable = false;
+}
+
+void MOTT::SetTxPin(int pin)
+{
+	TX_SIGNAL_PIN = pin;
+}
+
+void MOTT::SetRxPin(int pin)
+{
+	RX_SIGNAL_PIN = pin;
+}
+
+void MOTT::HandleInterrupt()
+{
+	if(sending) {
+		SendBit();
+	}else if(reading_signal){
+		ReadBit();
+	}
+	
+}
+
+int MOTT::CreateBitSignalFromCharArray(char* string)
+{
+	
+  int len = strlen(string);
+	
+  int BitLength = (len+1)*7 + 1;
+
+  bool auxiliar_array[7];
+
+  //Flag para recepcion
+  signal[0] = 1;
+
+  //Codificar el largo del payload en bits
+  ConvertCharToBoolArray((char) len, auxiliar_array);
+  
+  for(int i = 1; i < 8; i++){
+    signal[i] = auxiliar_array[i-1];
+  }
+
+  //Codificar el payload en bits
+  for(int h = 1; h <= len; h++)
+  {
+    ConvertCharToBoolArray((char) string[h-1], auxiliar_array);
+
+    for(int j = h*7 + 1; j < h*7 + 8; j++)
+    {
+      signal[j] = auxiliar_array[j - h*7 - 1];
+    }   
+  }
+  
+  SIGNAL_SIZE = BitLength;
+  
+  return BitLength;
+}
+
+void MOTT::SendSignal(char* string)
+{
+	if(sending == true || reading_signal == true) return;
+	
+	CreateBitSignalFromCharArray(string);
+	sending = true;
+	Timer1.restart();
+}
+
+void MOTT::SendBit()
+{
+  if(i < SIGNAL_SIZE)
+  {
+    digitalWrite(TX_SIGNAL_PIN,signal[i]);
+    //Serial.println(signal[i]);
+  }
+  
+  i++;
+
+  if(i == SIGNAL_SIZE + 1) 
+  {
+      sending = false;
+      i = 0;
+      digitalWrite(TX_SIGNAL_PIN,0);
+      Timer1.stop();
+      
+  } 
+  
+}
+
+void MOTT::SetBitTime(unsigned long time_in_ms, void (*f)())
+{
+  Timer1.attachInterrupt(f) ;
+  
+  TIMER_TIME = time_in_ms * ms;
+
+  Timer1.initialize(TIMER_TIME);  
+
+  Timer1.stop();
+}
+
+
+void MOTT::ConvertCharToBoolArray(char c, bool newSignal[])
+{
+  for(int i = 0; i < 7; i++)
+  {
+    newSignal[6-i] = (c >> i) & 0b1;
+  }
+} 
+
+//----------Sampling-------------------------------//
+
+char MOTT::ConvertBoolArrayToChar(bool arr[])
+{
+
+  char ByteSignal = arr[0] * 64;
+
+  int h = 1;
+  for(int j = 32; j > 0 ; j = j/2)
+  {
+    ByteSignal += arr[h] * j;
+    h++;
+  }
+
+  return ByteSignal;
+
+}
+
+void MOTT::SplitSignalIntoArrays(char* string)
+{
+
+  bool auxiliar_array[7];
+
+  //char string[largo];
+
+  for(int h = 0; h < largo; h++)
+  {
+  
+    for(int j = h*7; j < h*7 + 7; j++)
+    {
+      auxiliar_array[ j - h*7 ] = signal[j];
+    }
+
+    string[h] = ConvertBoolArrayToChar(auxiliar_array);
+
+  }
+
+  string[largo] = 0;
+  //Serial.println(string);
+  
+}
+
+void MOTT::ReadBit()
+{
+  if(i == 0)
+  {
+    largo = 0;
+  }
+
+  //Obtener el largo en bytes
+  if(i > 0 && i < 8)
+  {
+    largo += (1 << (7 - i)) * digitalRead(RX_SIGNAL_PIN);
+  }
+
+  //Setear la variable al largo en bits
+  if(i == 8)
+  {
+    SIGNAL_SIZE = largo * 7;
+  }
+
+  //Leer el payload
+  if(i >= 8)
+  {
+    signal[i-8] = digitalRead(RX_SIGNAL_PIN);
+  }
+
+
+  i++;
+
+  //Finaliza la lectura
+  if(i == SIGNAL_SIZE + 1 + 7)
+  {
+    reading_signal = false;
+    reading_ended = false;
+
+    SIGNAL_SIZE = SIGNAL_MAX_SIZE;
+    i = 0;
+    Timer1.stop();
+  }
+}
+
+void MOTT::BeginSamplingTimer()
+{
+
+	if(sending == true) return;
+	//Delay para muestrear en el centro del simbolo
+	delay(TIMER_TIME_IN_MS / 4);
+	Timer1.restart();  
+	reading_signal = true;
+}
+
+void MOTT::SampleSignalIfDetected()
+{
+
+	if(!samplingEnable) return;
+
+	if(reading_signal == false)
+	{
+		int sample = digitalRead(RX_SIGNAL_PIN);
+
+		if(IsSignalDetected(sample))
+		{
+		  Serial.println("Recibo signal");
+		  Serial.println("------------------");
+		  BeginSamplingTimer();
+		}
+       
+  }
+}
+
+bool MOTT::IsSignalDetected(int sample)
+{
+  return sample == 1 && reading_ended;
+}
+
+void MOTT::WaitForSignalEndingAfterInterrupt(char* string)
+{
+   if(reading_ended == false)
+  {
+	Serial.println("Entro a devolver el sampling");
+
+    SplitSignalIntoArrays(string);
+    //Delay para asegurar de que el pin este quieto pasado la onda
+    delay(TIMER_TIME_IN_MS);
+    reading_ended = true;
+
+  }
+}
+
+bool MOTT::SamplingEnded()
+{
+	SampleSignalIfDetected();
+	
+	return reading_ended == false;
+}
+
+void MOTT::StartSampling()
+{
+	//Delay para asegurar de que el pin este quieto pasado la onda
+    delay(TIMER_TIME_IN_MS);
+    reading_ended = true;
+	reading_signal = false;
+
+}
+
+void MOTT::ObtainSample(char *string)
+{
+	SplitSignalIntoArrays(string);
+}
+
+void MOTT::EnableSampling()
+{
+	samplingEnable = true;
+}
+
+void MOTT::DisableSampling()
+{
+	samplingEnable = false;
+}
+
+
+
+
---
+++ MOTT.h
@@ -0,0 +1,63 @@
+#ifndef MOTT_h
+#define MOTT_h
+
+#include "Arduino.h"
+#include <TimerOne.h>
+
+
+class MOTT
+{
+	static unsigned const long ms = 1000L;
+	static unsigned const int SIGNAL_MAX_SIZE = 7*50;
+	
+	unsigned long TIMER_TIME;
+	unsigned long TIMER_TIME_IN_MS;
+
+	int SIGNAL_SIZE;
+	bool signal[SIGNAL_MAX_SIZE];
+	
+	int TX_SIGNAL_PIN;
+	int RX_SIGNAL_PIN;
+	
+	unsigned int i;
+	
+	bool sending;
+	
+	//---------------------//
+	int largo;
+	bool reading_signal;
+	bool reading_ended;
+	bool samplingEnable;
+  
+  private:
+	char ConvertBoolArrayToChar(bool arr[]);
+    void ConvertCharToBoolArray(char c, bool* newSignal);
+	int CreateBitSignalFromCharArray(char* string);
+	void SplitSignalIntoArrays(char* string);
+	bool IsSignalDetected(int sample);
+	void WaitForSignalEndingAfterInterrupt(char* string);
+	void BeginSamplingTimer();
+	void SendBit();
+	void ReadBit();
+	
+  public:
+    MOTT();    
+	void SetTxPin(int pin);
+	void SetRxPin(int pin);
+	void SetBitTime(unsigned long time_in_ms, void (*f)());
+	void SendSignal(char* string);
+	void SampleSignalIfDetected();
+	void ObtainSample(char *string);
+	void StartSampling();
+	bool SamplingEnded();
+	void EnableSampling();
+	void DisableSampling();
+	void HandleInterrupt();
+
+	
+	
+
+	
+};
+
+#endif
